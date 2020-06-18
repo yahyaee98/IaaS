cbd39d4c9d51cfc8a8c49afad196a49cff4eec57
Soheil Yahyaee <yahyaee.soheil@gmail.com>
2020-06-18 16:03:13 +0200

    Add tests for third party packages

---
+++ pkg/itunes/itunes_test.go
@@ -0,0 +1,35 @@
+package itunes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestItunesCallsRightUrl(t *testing.T) {
+	server := httptest.NewServer(
+		http.HandlerFunc(
+			func(rw http.ResponseWriter, req *http.Request) {
+				assert.Equal(
+					t,
+					"/?term=some_search&limit=1&entity=musicTrack",
+					req.URL.String(),
+				)
+				_, _ = rw.Write([]byte(`{}`))
+			},
+		),
+	)
+	defer server.Close()
+
+	gb := &Itunes{
+		resultLimit: 1,
+		baseUrl:     server.URL,
+		client:      server.Client(),
+		log: func(msg string, keysAndValues ...interface{}) {
+
+		},
+	}
+
+	_, _ = gb.Search("some_search")
+}
